0e903e644ce9b8ec1dcd131092a3cbdddfce65c9
mumbereh <siirasonh@gmail.com>
2023-10-24 08:51:06 +0300

    Modified musicAlbum.rb class

--- classes/musicAlbum.rb
+++ classes/musicAlbum.rb
@@ -2,16 +2,14 @@ require 'date'
 require_relative 'item'
 
 class MusicAlbum < Item
-  attr_accessor :on_spotify
-
-  def initialize(publish_date, on_spotify, archived: false)
-    super(publish_date, archived: archived)
-    @on_spotify = on_spotify
+  def initialize(publish_date, archived: false)
+    super(publish_date)
+    @archived = archived
   end
 
   def can_be_archived?
     super_result = super
-    puts "In MusicAlbum: super: #{super_result} @on_spotify: #{@on_spotify} result: #{super_result && @on_spotify}"
-    super_result && @on_spotify
+    puts "In MusicAlbum: super: #{super_result} result: #{super_result && @archived}"
+    super_result && @archived
   end
 end
