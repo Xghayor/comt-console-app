6e2e874df99f2b59ffcf5650419d265b7b1ec982
firstpj <114886262+firstpj@users.noreply.github.com>
2023-10-26 00:52:54 +0100

    update

--- app.rb
+++ app.rb
@@ -1,8 +1,15 @@
+require_relative 'classes/book'
+require_relative 'classes/author'
+require_relative 'utils/create'
+require_relative 'utils/List'
+
 class App
+  include Create
+  include List
   attr_accessor :books, :music, :games, :genres, :labels, :authors
 
   def initialize
-    @book = []
+    @books = []
     @music = []
     @games = []
     @genres = []
@@ -11,10 +18,10 @@ class App
   end
 
   def actions(option)
-    method = ['Book added', 'Music added', 'Game added', 'All books', 'All music albums', 'All games',
-              'All genres', 'All labels', 'All authors']
+    methods = [method(:create_book), 'Music added', method(:create_game), method(:list_books), 'All music albums',
+               method(:list_games), 'All genres', method(:list_labels), method(:list_authors)]
     if (1..10).include?(option)
-      puts method[option - 1]
+      methods[option - 1].call
     else
       puts 'Choose a number between 1 and 10'
     end
