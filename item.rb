596b7bc3f5b70b5cf99555acb249fccbcfea0eaf
firstpj <114886262+firstpj@users.noreply.github.com>
2023-10-25 16:55:55 +0100

    author.json, games.json and item.rb file

--- item.rb
+++ item.rb
@@ -1,12 +1,26 @@
+require 'date'
 class Item
-  attr_accessor :name, :author
-
-  def initialize(name)
-    @name = name
-    @author = nil
+  attr_reader :genre, :publish_date, :label, :author
+ 
+  def initialize(publish_date)
+    @id = rand(1..1000)
+    @publish_date = publish_date
+    @archived = false
+  end
+  def move_to_archive
+    @archived = can_be_archived?
   end
 
-  def author=(author)
+  def can_be_archived?
+    years_since_published_date >= 10
+  end
+
+  def years_since_published_date
+    Date.today.year - @publish_date.year
+  end
+
+  def author = (author)
     @author = author
-  end
+    author.add_item(self)
+ end
 end
