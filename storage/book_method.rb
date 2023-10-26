aec4737d696c0277e77d791e2781bcaa2ca21fbe
mumbereh <siirasonh@gmail.com>
2023-10-26 10:39:13 +0300

    Update

--- storage/book_method.rb
+++ storage/book_method.rb
@@ -6,7 +6,7 @@ module BooksDataController
   def load_books
     file = './json_files/books.json'
     data = []
-    if File.exist?(file) && File.read(file) != ''
+    if File.exist?(file) && !File.empty?(file)
       JSON.parse(File.read(file)).each do |element|
         data.push(Book.new(element['name'], element['publisher'], element['cover_state'], element['published_date']))
       end
