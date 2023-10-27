8a3ce27d44adef7faaec5d6f70f2ec817d459ffc
Xghayor <cc.baqir@gmail.com>
2023-10-27 09:59:25 +0100

    fix bugs

--- music_album.rb
+++ music_album.rb
@@ -4,6 +4,7 @@ class MusicAlbum < Item
   attr_accessor :name, :published_date, :on_spotify
 
   def initialize(name, published_date, on_spotify)
+    super(id, name, published_date)
     @name = name
     @published_date = published_date
     @on_spotify = on_spotify
