e9b74375d8f8d9eabed1e0bf7de13a43cbb2b4fa
Xghayor <cc.baqir@gmail.com>
2023-10-27 10:18:25 +0100

    fix bugs

--- music_album.rb
+++ music_album.rb
@@ -1,16 +1,15 @@
 require_relative 'item'
 
 class MusicAlbum < Item
-  attr_accessor :name, :published_date, :on_spotify
-
+  attr_reader :on_spotify
+  
   def initialize(name, published_date, on_spotify)
     super(id, name, published_date)
     @name = name
-    @published_date = published_date
     @on_spotify = on_spotify
   end
 
   def can_be_archived?
-    super && @on_spotify == true
+    super and @on_spotify == true
   end
 end
