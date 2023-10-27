826b22924f7372f8699fa2d72d21ca33ce73d6fb
Xghayor <cc.baqir@gmail.com>
2023-10-27 10:20:08 +0100

    fix music_album linters errors

--- music_album.rb
+++ music_album.rb
@@ -2,7 +2,7 @@ require_relative 'item'
 
 class MusicAlbum < Item
   attr_reader :on_spotify
-  
+
   def initialize(name, published_date, on_spotify)
     super(id, name, published_date)
     @name = name
